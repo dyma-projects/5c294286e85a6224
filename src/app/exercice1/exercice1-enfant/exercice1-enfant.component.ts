a5b6d501ed0b3089cc650cf0c30202942106b98c
orphen92 <orphen92300@gmail.com>
2019-01-09 12:37:10 +0100

    End of project 3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
-
+  @Input() compteur: number;
+  @Output() newCompteurVal: EventEmitter<number> = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  updateCompteur(currentVal: number, incrementationWay: string): void {
+    if (incrementationWay === 'more') {
+      this.newCompteurVal.emit(currentVal + 1);
+    } else {
+      this.newCompteurVal.emit(currentVal - 1);
+    }
+  }
 }
--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice2.component.css']
 })
 export class Exercice2Component implements OnInit {
+  @ViewChild('input') input: ElementRef;
   public valeur: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getValue(): void {
+    this.valeur = this.input.nativeElement.value;
   }
 
 }
